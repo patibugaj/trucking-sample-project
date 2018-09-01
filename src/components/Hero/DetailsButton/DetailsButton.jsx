572224037e8af4ad55d177656cc0c44062be86ef
Patrycja Dzaluk <patrycja.dzaluk@gmail.com>
2018-09-01 12:19:10 +0200

    Add view details button in Hero section

--- src/components/Hero/DetailsButton/DetailsButton.jsx
+++ src/components/Hero/DetailsButton/DetailsButton.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+const Button = styled.button`
+  background-color: rgb(253,195,0);
+  color: #fff;
+  font-family: 'Raleway',sans-serif;
+  font-size: 13px;
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 22px 63px 18px 63px;
+  border-radius: 30px;
+  border-style: none;
+  margin: 50px 0;
+`
 
 const DetailsButton = () => (
-  <div>
-    <h2>DetailsButton</h2>
-  </div>
+  <Container>
+    <Button>View Details</Button>
+  </Container>
 )
 
 export default DetailsButton
