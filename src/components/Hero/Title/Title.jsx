f9d4ece6d0dcd2de58fc635a66950b64218a8465
Patrycja Dzaluk <patrycja.dzaluk@gmail.com>
2018-09-01 10:24:25 +0200

    Add main title in Hero section

--- src/components/Hero/Title/Title.jsx
+++ src/components/Hero/Title/Title.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: 154px 0;
+`
+
+const Header = styled.h1`
+  font-family: 'Raleway', sans-serif;
+  font-weight: bolder;
+  font-size: 72px;
+  text-transform: uppercase;
+  color: #fff;
+  display: table-caption;
+  line-height: 60px;
+
+  ::before {
+    content: '';
+    border: 2px solid rgb( 253, 195, 0);
+    width: 66px;
+    display: block;
+    margin: 0 0 20px 5px;
+
+  }
+`
 
 const Title = () => (
-  <div>
-    <h2>Title</h2>
-  </div>
+  <Container>
+    <Header>Strongest Distribiution Network</Header>
+  </Container>
 )
 
 export default Title
