39f58607cc96ef768024b454c7994ab6c4bccdaf
Patrycja Dzaluk <patrycja.dzaluk@gmail.com>
2018-09-02 20:31:22 +0200

    Fix windows ignorecase issue

--- src/components/Hero/NavBar/NavBar.jsx
+++ src/components/Hero/NavBar/NavBar.jsx
@@ -10,11 +10,10 @@ const Container = styled.div`
   margin-top: 42px;
   background-color: #fff;
   border-radius: 3px;
-  outline: 1px solid black;
-  box-shadow: 0px -8px 0px rgb(65, 81, 94);
+  box-shadow: 0px -8px 0px #41515e;
   display: flex;
   flex-direction: row;
-  wrap: no-wrap;
+  flex-wrap: no-wrap;
   justify-content: flex-end;
 `
 
